1b9d8f048d66c2b139b8a3d4038d60b63c272d78
Daisuke Yamazaki <ymz.ptr@gmail.com>
2022-02-13 22:08:46 +0900

    Considered surface connectivity

--- app.py
+++ app.py
@@ -3,39 +3,39 @@ import ghhops_server as hs
 import rhino3dm as rh
 import numpy as np
 
-from tessellate import transform_from3pt, tessellate_2d
+from tessellate import tessellate_mesh
 
 # register hops app as middleware
 app = Flask(__name__)
 hops: hs.HopsFlask = hs.Hops(app)
+
 
 # flask app can be used for other stuff directly
 @app.route("/help")
 def help():
     return "Welcome to Grashopper Hops for CPython!"
 
-def scipytriangle_to_rhinomesh_transform(tri, tr:np.ndarray) -> rh.Mesh:
-    """2d Delaunay Mesh with Scipy to 3d Rhino Mesh
+
+def triangles_to_rhinomesh_transform(vertices, triangles) -> rh.Mesh:
+    """節点と面-節点関係からRhinoMeshを作成
 
     Args:
-        tri (): scipy output delaunay mesh
-        tr (4x4 ndarray): transform matrix(not inversed)
+        vertices ([N] array-like: Vertex): N vertices 3d coordinate
+        triangles ([3,M] array-like: int): M facet corner vertices index
 
     Returns:
-        rh.Mesh: 
+        rhino3dm.Mesh: Converted Mesh
     """
     mesh = rh.Mesh()
-    invtr = np.linalg.inv(tr)
-
     # register vertex
-    for pti in tri.points:
-        pt_vec = np.array([pti[0], pti[1], 0, 1]) @ invtr
-        mesh.Vertices.Add(pt_vec[0], pt_vec[1], pt_vec[2])
+    for pti in vertices:
+        mesh.Vertices.Add(pti.x, pti.y, pti.z)
     # register face
-    for simpl in tri.simplices:
+    for simpl in triangles:
         mesh.Faces.AddFace(simpl[0], simpl[1], simpl[2])
 
     return mesh
+
 
 @hops.component(
     "/tessellate",
@@ -50,32 +50,23 @@ def scipytriangle_to_rhinomesh_transform(tri, tr:np.ndarray) -> rh.Mesh:
     ],
 )
 def tessellate(crv: rh.Curve, pt: rh.Point3d) -> rh.Mesh:
-    
     pl = crv.TryGetPolyline()
     pts_num = len(pl)-1
 
-    marged_mesh = rh.Mesh()
-
+    vertices = [[pt.X, pt.Y, pt.Z]]
+    faces = []
     for i in range(pts_num):
+        vi1 = i + 1
+        vi2 = (i + 1) % pts_num + 1
+        faces.append([0, vi1, vi2])
+
         pti = pl[i]
-        pt_next = pl[(i+1)%pts_num]
+        vertices.append([pti.X, pti.Y, pti.Z])
 
-        pt00 = np.array([pt.X, pt.Y, pt.Z, 1.0])
-        pt11 = np.array([pti.X, pti.Y, pti.Z, 1.0])
-        pt22 = np.array([pt_next.X, pt_next.Y, pt_next.Z, 1.0])
-        
-        matE = transform_from3pt(pt00[:3], pt11[:3], pt22[:3])
-        p1 = pt00 @ matE
-        p2 = pt11 @ matE
-        p3 = pt22 @ matE
+    ref_vertices, ref_triangles = \
+        tessellate_mesh(np.array(vertices), np.array(faces), 20.0)
 
-        poly = np.array([[p1[0], p1[1]], [p2[0], p2[1]], [p3[0], p3[1]]])
-        tri = tessellate_2d(poly, 20.0)
-        mesh_i = scipytriangle_to_rhinomesh_transform(tri, matE)
-
-        marged_mesh.Append(mesh_i)
-
-    return marged_mesh
+    return triangles_to_rhinomesh_transform(ref_vertices, ref_triangles)
 
 
 if __name__ == "__main__":
--- tessellate.py
+++ tessellate.py
@@ -1,10 +1,11 @@
+import math
+import hashlib
+
 import numpy as np
 import scipy.spatial as ss
-import random
-import math
 
 
-def transform_from3pt(pt0, pt1, pt2):
+def transform_matrix_3pt(pt0, pt1, pt2):
     matB = np.identity(4)
     e1 = pt1 - pt0
     e1 /= np.linalg.norm(e1)
@@ -24,23 +25,30 @@ def transform_from3pt(pt0, pt1, pt2):
     return matT @ matB
 
 
-def sqdistance(pt1, pt2):
-    return math.sqrt((pt1[0] - pt2[0])**2 + (pt1[1] - pt2[1])**2)
+def distance_2pt(pt1, pt2):
+    return math.sqrt(sum((pt1 - pt2)**2))
 
 
 def divideby_distance(pt1, pt2, reflength):
-    dist = sqdistance(pt1, pt2)
     vec12 = pt2 - pt1
-    div_num = int(dist/reflength) + 1
+    div_num = int(distance_2pt(pt1, pt2)/reflength) + 1
     vec12 = vec12 / div_num
-
     points = []
     for i in range(1, div_num):
         points.append(pt1 + i * vec12)
-        return points
+    return points
 
 
 def randompts_inside_polygon(num: int, poly):
+    """Create random points inside input polygon
+
+    Args:
+        num (int): number of create points
+        poly ((dim,n)ndarray): polygon corner points coordinate.
+
+    Returns:
+        points [(...,num) array-like]: random points inside polygon.
+    """
     pts_num = len(poly)
     points = []
 
@@ -53,38 +61,159 @@ def randompts_inside_polygon(num: int, poly):
             sum_pt += fac * pt
 
         points.append(sum_pt / sum_fac)
-    
+
     return points
 
 
-def tessellate_2d(poly, reflength: float):
-    """
-    Tessellate 2d comvex resion delaunay trianguration 
+class Vertex:
+    """Mesh Vertex
 
-    Args:
-        poly (2dim-ndarray): [[x0,y0],[x1,y1],...]
-        reflength (float): reference length, decide average density.
+    Attributes:
+        x, y, z (float): 頂点座標成分
     """
-    tess_points = []
-    pts_num = len(poly)
+    def __init__(self, x: float, y: float, z: float):
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def to_array(self):
+        return np.array([self.x, self.y, self.z])
+
+    def to_localcoord(self, trans_matrix):
+        """この頂点を2次元局所座標系へ変換
+
+        Args:
+            trans_matrix ((4,4)ndarray): 変換行列
+
+        Returns:
+            2次元局所座標[(2)ndarray]: trans_matrixに基づいて計算された2次元局所座標系
+        """
+        vec1 = np.array([self.x, self.y, self.z, 1.0])
+        return (vec1 @ trans_matrix)[:2]
+
+
+class TopologicalEdge:
+    """Topological Edge
+    順列を区別しないエッジの格納
+    """
+    def __init__(self, n1: int, n2: int):
+        self.n1 = n1
+        self.n2 = n2
+
+    def __hash__(self):
+        h1 = hashlib.sha224(hex(self.n1).encode()).digest()
+        h2 = hashlib.sha224(hex(self.n2).encode()).digest()
+        return hash(h1) + hash(h2)
+
+    def __eq__(self, other):
+        return hash(self) == hash(other)
+
+    def __str__(self):
+        return f"n1: {self.n1}, n2: {self.n2}"
+
+
+def tessellate_mesh(vertices, faces, reflength):
+    """Delaunay based refinement to input mesh
+
+    Args:1
+        vertices ((n,dim)ndarray): [[x1, y1, z1], [x2, y2, z2]...], float
+        faces ((m,...)ndarray): [[n1_1, n2_1, n3_1], [n1_2, n2_2, n3_2]...]
+
+    Notes:
+        n: Vertices number
+        m: Faces number
+
+    Returns:
+        tess_vertices [(n) array-like: Vertex]: 生成メッシュの頂点
+        tess_triangles [(3,n) array-like: int]: 生成メッシュの頂点-頂点関係
+    """
+    # Initialize tessellated verticies collection
     refarea = reflength**2 * 0.5
+    tess_vertices = []  # Result vertices collection
+    tess_triangles = []  # Result triangle-vertices relation collection
+    for pti in vertices:
+        tess_vertices.append(Vertex(pti[0], pti[1], pti[2]))
 
-    # Corner & Edge
-    for i, pti in enumerate(poly):
-        # register corner
-        tess_points.append(pti) 
+    # Final vertex ID indicator
+    vertex_count = len(tess_vertices)
 
-        # register edge (divided)
-        pt_next = poly[(i+1) % pts_num]
-        tess_points += divideby_distance(pti, pt_next, reflength)
+    # Extract topological edges
+    edge_set = set()
+    for fi in faces:
+        # collect edges
+        edge_set.add(TopologicalEdge(fi[0], fi[1]))
+        edge_set.add(TopologicalEdge(fi[1], fi[2]))
+        edge_set.add(TopologicalEdge(fi[2], fi[0]))
 
-    # Inside
-    # NOTE : ConvexPolygonの面積を簡単に求める方法忘れた。とりあえず三角形前提でゴリ押す。
-    # TODO : 思い出して密度に応じた点の生成を行う。
-    p0 = poly[0]; p1 = poly[1]; p2 = poly[2]
-    inside_pts_num = int(np.cross(p1-p0, p2-p0) * 0.5 / refarea)
-    inside_pts_num = max(inside_pts_num, 10)
-    
-    tess_points += randompts_inside_polygon(inside_pts_num, poly)
+    # Divide collected edges
+    edge_vertexids = {}
+    for ei in edge_set:
+        # 分割された節点を細分化節点コレクションに追加
+        mid_points = divideby_distance(vertices[ei.n1], vertices[ei.n2], reflength)
+        vertexids = []
+        for i, pti in enumerate(mid_points):
+            tess_vertices.append(Vertex(pti[0], pti[1], pti[2]))
+            vertexids.append(vertex_count)
+            vertex_count += 1
 
-    return ss.Delaunay(tess_points)
+        # 節点IDをそれぞれのエッジごとに保存
+        edge_vertexids[ei] = vertexids
+
+    # Facetごとの処理
+    for fi in faces:
+        face_vertex_ids = []  # 2次元座標の頂点に対応する3次元頂点のIDを格納
+        face_vertices = []  # 2次元座標の頂点を格納
+
+        # コーナー頂点を取得
+        v1 = tess_vertices[fi[0]]
+        v2 = tess_vertices[fi[1]]
+        v3 = tess_vertices[fi[2]]
+        # 頂点IDをコレクション
+        face_vertex_ids.append(fi[0])
+        face_vertex_ids.append(fi[1])
+        face_vertex_ids.append(fi[2])
+        # 局所平面座標系の変換マトリクスを取得
+        matE = transform_matrix_3pt(v1.to_array(), v2.to_array(), v3.to_array())
+        # コーナー頂点の二次元座標を計算して登録
+        p1 = v1.to_localcoord(matE)
+        p2 = v2.to_localcoord(matE)
+        p3 = v3.to_localcoord(matE)
+        face_vertices.append(p1)
+        face_vertices.append(p2)
+        face_vertices.append(p3)
+        # 面積から内部節点の生成数を決定
+        inside_pts_num = abs(int(np.cross(p2-p1, p3-p1) * 0.5 / refarea))
+
+        # エッジ頂点を取得
+        polygon_vertices = []
+        for j in range(3):
+            # 三角形の頂点座標をコレクション
+            polygon_vertices.append(vertices[fi[j]])
+            # エッジ上の頂点を取得してIDをコレクション
+            vertexids_onedge = edge_vertexids[TopologicalEdge(fi[j], fi[(j+1) % 3])]
+            # エッジ上の頂点の二次元座標を計算して登録
+            for k in vertexids_onedge:
+                face_vertex_ids.append(k)
+                face_vertices.append(tess_vertices[k].to_localcoord(matE))
+
+        # 内部節点の生成
+        for pti in randompts_inside_polygon(inside_pts_num, np.array(polygon_vertices)):
+            # 内部節点を生成して細分化節点コレクションに追加
+            vi = Vertex(pti[0], pti[1], pti[2])
+            tess_vertices.append(vi)
+            # 節点IDをコレクション
+            face_vertex_ids.append(vertex_count)
+            vertex_count += 1
+            # 生成節点を二次元座標に変換して登録
+            face_vertices.append(vi.to_localcoord(matE))
+
+        # Delaunay分割
+        delaun_result = ss.Delaunay(face_vertices)
+
+        # 単体(3角形)の節点IDを全体節点IDに変換してコレクション
+        for facet in delaun_result.simplices:
+            tess_triangles.append([face_vertex_ids[facet[0]],
+                                   face_vertex_ids[facet[1]],
+                                   face_vertex_ids[facet[2]]])
+
+    return tess_vertices, tess_triangles
